26b14a7c04968f91235913eae5fd52aa534c867c
tonymushah <tonyramanantsalama@gmail.com>
2026-04-05 19:42:01 +0300

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,13 @@
 use log::warn;
 
+pub(crate) fn todo_warn_of_something(something: &'static str) {
+    if something.is_empty() {
+        warn!("Not yet implemented")
+    } else {
+        warn!("Not yet implemented {something}")
+    }
+}
+
 pub(crate) fn todo_warn() {
-    warn!("Not yet implemented")
+    todo_warn_of_something("");
 }

91e5c29e7bbcaae5304ecb8f13012d5d39912dfa
tonymushah <tonyramanantsalama@gmail.com>
2026-04-05 23:42:37 +0300

    Update run.rs

--- examples/run.rs
+++ examples/run.rs
@@ -1,4 +1,21 @@
+use masonry::{
+    core::{NewWidget, Properties, Widget},
+    palette::css,
+    properties::ContentColor,
+    widgets::Label,
+};
+use velona::window::WindowBuilder;
+
+fn view() -> NewWidget<dyn Widget + 'static> {
+    Label::new("Text")
+        .with_props(Properties::one(ContentColor::new(css::WHITE)))
+        .erased()
+}
+
 fn main() {
     env_logger::init();
-    velona::app::Builder::default().run().unwrap()
+    velona::app::Builder::default()
+        .window(WindowBuilder::new(view).with_title("aaaaaa"))
+        .run()
+        .unwrap()
 }

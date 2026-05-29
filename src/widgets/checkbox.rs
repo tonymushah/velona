b9f7cd9a87f28197f28660d48b67d4deadf45a01
Dirk Tony <tonydirk.smsconsulting@gmail.com>
2026-05-29 11:00:25 +0300

    implmented checkbox extension trait

--- src/widgets/checkbox.rs
+++ src/widgets/checkbox.rs
@@ -1,3 +1,67 @@
+use masonry::{
+    core::{ArcStr, NewWidget, Widget},
+    widgets::{Checkbox, CheckboxToggled},
+};
+use reactive_graph::{computed::Memo, graph::untrack, signal::WriteSignal, traits::Get};
+
+use crate::{NewWidgetExt, widgets::checkbox};
+
+/// A [`Checkbox`] trait extension
 pub trait NewCheckboxExt {
-    
+    /// create a new checkbox with reactive text and checked value
+    fn new<Cf, Tf, T>(checked: Cf, text: Tf) -> Self
+    where
+        Cf: Fn() -> bool + 'static,
+        Tf: Fn() -> T + 'static,
+        T: Into<ArcStr>;
+    /// Make the `checked` value reactive
+    fn checked<C>(self, checked: C) -> Self
+    where
+        C: Fn() -> bool + 'static;
+    /// Make the `checked` value reactive that warps `checked` with a [`Memo`].
+    fn checked_memozied<C>(self, checked: C) -> Self
+    where
+        C: Fn() -> bool + Send + 'static + Sync,
+        Self: std::marker::Sized,
+    {
+        let checked_memo = Memo::new(move |_| checked());
+        self.checked(move || checked_memo.get())
+    }
+    /// Make the `text` value reactive
+    fn text<Tf, T>(self, text: Tf) -> Self
+    where
+        Tf: Fn() -> T + 'static,
+        T: Into<ArcStr>;
+}
+
+impl NewCheckboxExt for NewWidget<Checkbox> {
+    fn new<Cf, Tf, T>(checked: Cf, text: Tf) -> Self
+    where
+        Cf: Fn() -> bool + 'static,
+        Tf: Fn() -> T + 'static,
+        T: Into<ArcStr>,
+    {
+        Checkbox::new(untrack(&checked), untrack(&text))
+            .with_auto_id()
+            .checked(checked)
+            .text(text)
+    }
+    fn checked<C>(self, checked: C) -> Self
+    where
+        C: Fn() -> bool + 'static,
+    {
+        self.use_reactive_widget_mut(move |mut widget_mut| {
+            Checkbox::set_checked(&mut widget_mut, checked());
+        })
+    }
+
+    fn text<Tf, T>(self, text: Tf) -> Self
+    where
+        Tf: Fn() -> T + 'static,
+        T: Into<ArcStr>,
+    {
+        self.use_reactive_widget_mut(move |mut widget_mut| {
+            Checkbox::set_text(&mut widget_mut, text().into());
+        })
+    }
 }

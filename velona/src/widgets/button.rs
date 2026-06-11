697603f17d93d214b78d87e918fe63149f4a386a
Dirk Tony <tonydirk.smsconsulting@gmail.com>
2026-06-11 11:35:55 +0300

    Update button.rs

--- velona/src/widgets/button.rs
+++ velona/src/widgets/button.rs
@@ -79,105 +79,137 @@ btn_ev_trait!(
     },
 
     Some(PointerButton::Primary)  => {
+        /// Primary button, commonly the left mouse button, touch contact, pen contact.
         on_primary
     },
     Some(PointerButton::Secondary)  => {
+        /// Secondary button, commonly the right mouse button, pen barrel button.
         on_secondary
     },
     Some(PointerButton::Auxiliary)  => {
+        /// Auxiliary button, commonly the middle mouse button.
         on_auxiliary
     },
 
     Some(PointerButton::X1)  => {
+        /// X1 (back) Mouse.
         on_x1
     },
     Some(PointerButton::X2)  => {
+        /// X2 (forward) Mouse.
         on_x2
     },
 
     Some(PointerButton::PenEraser)  => {
+        /// Pen erase button.
         on_pen_eraser
     },
 
     Some(PointerButton::B7)  => {
+        /// Button 7.
         on_b7
     },
     Some(PointerButton::B8)  => {
+        /// Button 8.
         on_b8
     },
     Some(PointerButton::B9)  => {
+        /// Button 9.
         on_b9
     },
 
     Some(PointerButton::B10)  => {
+        /// Button 10.
         on_b10
     },
     Some(PointerButton::B11)  => {
+        /// Button 11.
         on_b11
     },
     Some(PointerButton::B12)  => {
+        /// Button 12.
         on_b12
     },
     Some(PointerButton::B13)  => {
+        /// Button 13.
         on_b13
     },
     Some(PointerButton::B14)  => {
+        /// Button 14.
         on_b14
     },
     Some(PointerButton::B15)  => {
+        /// Button 15.
         on_b15
     },
     Some(PointerButton::B16)  => {
+        /// Button 16.
         on_b16
     },
     Some(PointerButton::B17)  => {
+        /// Button 17.
         on_b17
     },
     Some(PointerButton::B18)  => {
+        /// Button 18.
         on_b18
     },
     Some(PointerButton::B19)  => {
+        /// Button 19.
         on_b19
     },
 
     Some(PointerButton::B20)  => {
+        /// Button 20.
         on_b20
     },
     Some(PointerButton::B21)  => {
+        /// Button 21.
         on_b21
     },
     Some(PointerButton::B22)  => {
+        /// Button 22.
         on_b22
     },
     Some(PointerButton::B23)  => {
+        /// Button 23.
         on_b23
     },
     Some(PointerButton::B24)  => {
+        /// Button 24.
         on_b24
     },
     Some(PointerButton::B25)  => {
+        /// Button 25.
         on_b25
     },
     Some(PointerButton::B26)  => {
+        /// Button 26.
         on_b26
     },
     Some(PointerButton::B27)  => {
+        /// Button 27.
         on_b27
     },
     Some(PointerButton::B28)  => {
+        /// Button 28.
         on_b28
     },
     Some(PointerButton::B29)  => {
+        /// Button 29.
         on_b29
     },
 
     Some(PointerButton::B30)  => {
+        /// Button 30.
         on_b30
     },
     Some(PointerButton::B31)  => {
+        /// Button 31.
         on_b31
     },
     Some(PointerButton::B32)  => {
+        /// Button 32.
         on_b32
     },
 );

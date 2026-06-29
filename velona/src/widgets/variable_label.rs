34de1268f7d9cb83d55d29bb8c55a7e0d4a2b417
Dirk Tony <tonyramanantsalama@gmail.com>
2026-06-29 08:30:57 +0300

    added `impl NewLabelExt for NewWidget<VariableLabel>`

--- velona/src/widgets/variable_label.rs
+++ velona/src/widgets/variable_label.rs
@@ -1,12 +1,18 @@
+use std::{
+    mem::{Discriminant, discriminant},
+    sync::{self, Mutex},
+};
+
 use masonry::{
-    core::{NewWidget, WidgetMut},
+    TextAlign,
+    core::{ArcStr, NewWidget, StyleProperty, WidgetMut},
     widgets::{Label, VariableLabel},
 };
 
 #[cfg(doc)]
 use reactive_graph::effect::Effect;
 
-use crate::NewWidgetExt;
+use crate::{NewWidgetExt, widgets::label::NewLabelExt};
 
 /// A utility struct for [`VariableLabel::set_target_weight`].
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -53,6 +59,69 @@ impl NewVariableLabelExt for NewWidget<VariableLabel> {
     {
         self.use_reactive_widget_mut(move |mut this| {
             target_weight().apply(&mut this);
+        })
+    }
+}
+
+impl NewLabelExt for NewWidget<VariableLabel> {
+    fn text<S, T>(self, text: S) -> Self
+    where
+        S: Fn() -> T + 'static,
+        T: Into<ArcStr>,
+    {
+        self.use_label_mut(move |mut this| {
+            Label::set_text(&mut this, text());
+        })
+    }
+
+    fn style_opt<S, T>(self, style: S) -> Self
+    where
+        S: Fn() -> Option<T> + 'static,
+        T: Into<StyleProperty>,
+    {
+        let old_style_data = sync::Arc::new(Mutex::new(None::<Discriminant<StyleProperty>>));
+        self.use_label_mut(move |mut this| {
+            let mut old_style_lock = {
+                if old_style_data.is_poisoned() {
+                    old_style_data.clear_poison();
+                }
+                old_style_data.lock().unwrap()
+            };
+            if let Some(old_style) = *old_style_lock {
+                Label::remove_style(&mut this, old_style);
+            }
+            if let Some(style) = style() {
+                *old_style_lock = Label::insert_style(&mut this, style)
+                    .as_ref()
+                    .map(discriminant);
+            } else {
+                *old_style_lock = None;
+            }
+        })
+    }
+    fn style<S, T>(self, style: S) -> Self
+    where
+        S: Fn() -> T + 'static,
+        T: Into<StyleProperty>,
+    {
+        self.style_opt(move || Some(style()))
+    }
+
+    fn hint<S>(self, hint: S) -> Self
+    where
+        S: Fn() -> bool + 'static,
+    {
+        self.use_label_mut(move |mut this| {
+            Label::set_hint(&mut this, hint());
+        })
+    }
+
+    fn text_alignment<S>(self, align: S) -> Self
+    where
+        S: Fn() -> TextAlign + 'static,
+    {
+        self.use_label_mut(move |mut this| {
+            Label::set_text_alignment(&mut this, align());
         })
     }
 }

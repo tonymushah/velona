1a57fbf25544117cc8509d21b536d29bf6cabe51
Dirk Tony <tonyramanantsalama@gmail.com>
2026-07-06 07:28:35 +0300

    Update `radio.rs`
    - add Documentation
    - remove `text` from `NewRadioExt`
    - implement `NewLabelExt` for `NewWidget<Radion>`

--- velona/src/widgets/radio.rs
+++ velona/src/widgets/radio.rs
@@ -1,30 +1,36 @@
+//! Various [`RadioButton`] implementations
+//!
+//! The most important thing in the module is the [`NewRadioButtonExt`]
+//! which is implemented for [`NewWidget<RadioButton>`].
+//!
+//! [`NewWidget<Radio>`] also implement the [`NewLabelExt`] trait
+//! (since a [radio](RadioButton) is just a [label](label) wrapper).
+//!
+//! _See the [widget](RadioButton) documentation for more information_.
+
+use std::mem::{Discriminant, discriminant};
+
 use masonry::{
-    core::{ArcStr, NewWidget, WidgetMut},
+    TextAlign,
+    core::{ArcStr, NewWidget, StyleProperty, WidgetMut},
     widgets::{Label, RadioButton},
 };
 
 #[cfg(doc)]
 use reactive_graph::effect::Effect;
 
-use crate::NewWidgetExt;
+use crate::{NewWidgetExt, widgets::label::NewLabelExt};
 
 /// A [new](NewWidget) [`RadioButton`] extension trait.
-// TODO add example
 pub trait NewRadioButtonExt {
     /// [Check or uncheck the box](RadioButton::set_checked) reactively.
     fn checked<C>(self, checked: C) -> Self
     where
         C: Fn() -> bool + 'static;
-    /// [Set the text](RadioButton::set_text) reactively.
-    ///
-    /// We enforce this to be an ArcStr to make the allocation explicit.
-    fn text<F>(self, text: F) -> Self
-    where
-        F: Fn() -> ArcStr + 'static;
     /// Use a mutable reference to the label.
     ///
     /// It is worth noting that the `use_fn` runs inside an [`Effect`].
-    fn label_mut<F>(self, use_fn: F) -> Self
+    fn use_label_mut<F>(self, use_fn: F) -> Self
     where
         F: FnMut(WidgetMut<Label>) + 'static;
 }
@@ -39,21 +45,74 @@ impl NewRadioButtonExt for NewWidget<RadioButton> {
         })
     }
 
-    fn text<F>(self, text: F) -> Self
-    where
-        F: Fn() -> ArcStr + 'static,
-    {
-        self.use_reactive_widget_mut(move |mut this| {
-            RadioButton::set_text(&mut this, text());
-        })
-    }
-
-    fn label_mut<F>(self, mut use_fn: F) -> Self
+    fn use_label_mut<F>(self, mut use_fn: F) -> Self
     where
         F: FnMut(WidgetMut<Label>) + 'static,
     {
         self.use_reactive_widget_mut(move |mut this| {
             use_fn(RadioButton::label_mut(&mut this));
+        })
+    }
+}
+
+impl NewLabelExt for NewWidget<RadioButton> {
+    fn text<S, T>(self, text: S) -> Self
+    where
+        S: Fn() -> T + 'static,
+        T: Into<ArcStr>,
+    {
+        self.use_label_mut(move |mut this| {
+            Label::set_text(&mut this, text());
+        })
+    }
+
+    fn style_opt<S, T>(self, style: S) -> Self
+    where
+        S: Fn() -> Option<T> + 'static,
+        T: Into<StyleProperty>,
+    {
+        self.use_reactive_widget_mut_with_effect_val::<_, Discriminant<StyleProperty>>(
+            move |mut this, old_style| {
+                let mut this = RadioButton::label_mut(&mut this);
+                if let Some(old_style) = old_style {
+                    Label::remove_style(&mut this, old_style);
+                }
+                if let Some(style) = style() {
+                    Label::insert_style(&mut this, style)
+                        .as_ref()
+                        .map(discriminant)
+                } else {
+                    None
+                }
+            },
+        )
+    }
+    fn style<S, T>(self, style: S) -> Self
+    where
+        S: Fn() -> T + 'static,
+        T: Into<StyleProperty>,
+    {
+        self.style_opt(move || Some(style()))
+    }
+
+    fn hint<S>(self, hint: S) -> Self
+    where
+        S: Fn() -> bool + 'static,
+    {
+        self.use_label_mut(move |mut this| {
+            Label::set_hint(&mut this, hint());
+        })
+    }
+
+    fn text_alignment<S>(self, align: S) -> Self
+    where
+        S: Fn() -> TextAlign + 'static,
+    {
+        // {
+        //     self.widget = Box::new(self.widget.with_text_alignment(untrack(&align)));
+        // }
+        self.use_label_mut(move |mut this| {
+            Label::set_text_alignment(&mut this, align());
         })
     }
 }

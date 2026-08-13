4ccfd1789db3aeaea51db7dd850904b95f21a174
Dirk Tony <tonyramanantsalama@gmail.com>
2026-08-13 18:47:53 +0300

    Update builder.rs
    Signed-off-by: Dirk Tony <tonyramanantsalama@gmail.com>

--- velona_core/src/window/builder.rs
+++ velona_core/src/window/builder.rs
@@ -189,7 +189,7 @@ impl WindowBuilder {
     /// The default is `None`.
     ///
     /// See [`Window::set_window_icon`](winit::window::Window::set_window_icon) for details.
-    pub fn with_window_icon<S>(self, window_icon: Option<winit::window::Icon>) -> Self {
+    pub fn with_window_icon(self, window_icon: Option<winit::window::Icon>) -> Self {
         self.update_window_attributes(|att| att.with_window_icon(window_icon))
     }
 }

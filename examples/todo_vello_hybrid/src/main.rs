9753c0cc38c54dca139cd94cb06f80dcac33cb11
Dirk Tony <tonyramanantsalama@gmail.com>
2026-09-05 17:03:25 +0300

    adding a portal on todo
    Signed-off-by: Dirk Tony <tonyramanantsalama@gmail.com>

--- examples/todo_vello_hybrid/src/main.rs
+++ examples/todo_vello_hybrid/src/main.rs
@@ -7,7 +7,7 @@ use velona::masonry::{
     layout::{AsUnit, Length},
     palette::css::{BEIGE, BLACK, WHITE},
     properties::{Background, BorderColor, BorderWidth, Padding},
-    widgets::{Button, Flex, FlexParams, Label, Prose, TextInput},
+    widgets::{Button, Flex, FlexParams, Label, Portal, Prose, TextInput},
 };
 use velona::reactive::{signal::signal, traits::Update};
 use velona::{
@@ -27,71 +27,77 @@ fn view() -> AnyNewWidget {
     //     trace!("todo real size: {}", size_of_val(&*todo_ref))
     // });
 
-    Flex::column()
-        .main_axis_alignment(masonry::properties::types::MainAxisAlignment::Center)
-        .cross_axis_alignment(masonry::properties::types::CrossAxisAlignment::Center)
-        .with_fixed(Prose::new("Todos").prepare())
-        .with_fixed(Flex::column().prepare().collect_reactive_iter(move || {
-            todos()
-                .into_iter()
-                .enumerate()
-                .map(move |(index, item)| {
-                    (
-                        Flex::row()
-                            .cross_axis_alignment(
-                                masonry::properties::types::CrossAxisAlignment::Center,
-                            )
-                            .main_axis_alignment(
-                                masonry::properties::types::MainAxisAlignment::Center,
-                            )
-                            .with_fixed(Label::new(item.clone()).prepare())
-                            .with_fixed_spacer(Length::px(20.0))
-                            .with_fixed(
-                                Button::with_text("Remove")
-                                    .prepare()
-                                    .on_primary(move || {
-                                        set_todos.update(|todos| {
-                                            todos.swap_remove(index);
-                                        });
-                                    })
-                                    .with_props(BorderColor::new(BLACK))
-                                    .with_props(Background::Color(BEIGE))
-                                    .with_props(BorderWidth::all(Length::px(1.0)))
-                                    .with_props(Padding::from_vh(4.0.px(), 12.0.px())),
-                            )
-                            .prepare()
-                            .erased(),
-                        FlexParams::default(),
-                    )
-                })
-                .collect::<Vec<_>>()
-        }))
-        .with_fixed(
-            TextInput::new("")
-                .with_placeholder("Put something...")
-                .prepare()
-                .with_props(BorderColor::new(BLACK))
-                .with_props(BorderWidth::all(Length::px(1.0)))
-                .with_props(Padding::from_vh(Length::px(4.0), Length::px(12.0)))
-                .on_text_action(move |a| match a {
-                    masonry::widgets::TextAction::Changed(e) => log::trace!("Changed input {e}"),
-                    masonry::widgets::TextAction::Entered(todo) => {
-                        set_todos.update(|todos| {
-                            if let Some(already) =
-                                todos.iter().find(|inside| &***inside == todo).cloned()
-                            {
-                                todos.push(already);
-                            } else {
-                                todos.push(todo.as_str().into());
-                            }
-                        });
-                    }
-                    _ => {}
-                }),
-        )
-        .prepare()
-        .with_props(Padding::all(Length::px(12.0)))
-        .erased()
+    Portal::new(
+        Flex::column()
+            .main_axis_alignment(masonry::properties::types::MainAxisAlignment::Center)
+            .cross_axis_alignment(masonry::properties::types::CrossAxisAlignment::Center)
+            .with_fixed(Prose::new("Todos").prepare())
+            .with_fixed(Flex::column().prepare().collect_reactive_iter(move || {
+                todos()
+                    .into_iter()
+                    .enumerate()
+                    .map(move |(index, item)| {
+                        (
+                            Flex::row()
+                                .cross_axis_alignment(
+                                    masonry::properties::types::CrossAxisAlignment::Center,
+                                )
+                                .main_axis_alignment(
+                                    masonry::properties::types::MainAxisAlignment::Center,
+                                )
+                                .with_fixed(Label::new(item.clone()).prepare())
+                                .with_fixed_spacer(Length::px(20.0))
+                                .with_fixed(
+                                    Button::with_text("Remove")
+                                        .prepare()
+                                        .on_primary(move || {
+                                            set_todos.update(|todos| {
+                                                todos.swap_remove(index);
+                                            });
+                                        })
+                                        .with_props(BorderColor::new(BLACK))
+                                        .with_props(Background::Color(BEIGE))
+                                        .with_props(BorderWidth::all(Length::px(1.0)))
+                                        .with_props(Padding::from_vh(4.0.px(), 12.0.px())),
+                                )
+                                .prepare()
+                                .erased(),
+                            FlexParams::default(),
+                        )
+                    })
+                    .collect::<Vec<_>>()
+            }))
+            .with_fixed(
+                TextInput::new("")
+                    .with_placeholder("Put something...")
+                    .prepare()
+                    .with_props(BorderColor::new(BLACK))
+                    .with_props(BorderWidth::all(Length::px(1.0)))
+                    .with_props(Padding::from_vh(Length::px(4.0), Length::px(12.0)))
+                    .on_text_action(move |a| match a {
+                        masonry::widgets::TextAction::Changed(e) => {
+                            log::trace!("Changed input {e}")
+                        }
+                        masonry::widgets::TextAction::Entered(todo) => {
+                            set_todos.update(|todos| {
+                                if let Some(already) =
+                                    todos.iter().find(|inside| &***inside == todo).cloned()
+                                {
+                                    todos.push(already);
+                                } else {
+                                    todos.push(todo.as_str().into());
+                                }
+                            });
+                        }
+                        _ => {}
+                    }),
+            )
+            .prepare()
+            .with_props(Padding::all(Length::px(12.0)))
+            .erased(),
+    )
+    .prepare()
+    .erased()
 }
 
 #[cfg_attr(feature = "hotpath", hotpath::main)]

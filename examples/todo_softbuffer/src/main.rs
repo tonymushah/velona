84d769d06543c117efd7974647206e77823a9fc7
Dirk Tony <tonyramanantsalama@gmail.com>
2026-09-06 11:11:30 +0300

    content must fill true
    Signed-off-by: Dirk Tony <tonyramanantsalama@gmail.com>

--- examples/todo_softbuffer/src/main.rs
+++ examples/todo_softbuffer/src/main.rs
@@ -100,6 +100,7 @@ fn view() -> AnyNewWidget {
             .with_props(Padding::all(Length::px(12.0)))
             .erased(),
     )
+    .content_must_fill(true)
     .prepare()
     .erased()
 }

60d98a92f8ac05505cc5302fc83882544948eb44
Dirk Tony <tonyramanantsalama@gmail.com>
2026-09-07 17:28:16 +0300

    Update event_listener.rs
    Signed-off-by: Dirk Tony <tonyramanantsalama@gmail.com>

--- velona_core/src/window/event_listener.rs
+++ velona_core/src/window/event_listener.rs
@@ -5,6 +5,7 @@ use std::{collections::HashMap, fmt::Debug};
 use log::{debug, warn};
 use masonry_core::core::{ErasedAction, Widget, WidgetId};
 use reactive_graph::owner::{Owner, on_cleanup};
+use winit::event::{DeviceId, KeyEvent, Modifiers, WindowEvent};
 
 use crate::{
     utils::events::{EventMap, NoParamHandler},
@@ -13,7 +14,11 @@ use crate::{
 
 pub use crate::utils::HandlerId;
 
-pub type HandlerFn = Box<dyn Fn(&ErasedAction) + Send>;
+pub type HandlerFn = HandlerFnGeneric<ErasedAction>;
+
+pub type HandlerFnGeneric<T> = Box<dyn Fn(&T) + Send>;
+
+pub type HandlerFnGenericStatic<T> = Box<dyn Fn(T) + Send>;
 
 pub type NoParamHandlerFn = NoParamHandler;
 
@@ -23,16 +28,40 @@ pub struct RegisterWindowEventHandler {
     pub type_: RegisterWindowEventHandlerType,
 }
 
+#[derive(Debug)]
+#[non_exhaustive]
+pub struct OnKeyboardInput {
+    pub device_id: DeviceId,
+    pub event: KeyEvent,
+    pub is_synthetic: bool,
+}
+
 #[derive(derive_more::Debug)]
 pub enum RegisterWindowEventHandlerType {
     Widget(WidgetId, #[debug(skip)] HandlerFn),
     OnDestroy(#[debug(skip)] NoParamHandlerFn),
+    OnFocused(#[debug(skip)] HandlerFnGenericStatic<bool>),
+    // breaking changes for this one see #130
+    // OnDropFile(#[debug(skip)] HandlerFnGeneric<>)
+    OnOccluded(#[debug(skip)] HandlerFnGenericStatic<bool>),
+    OnCursorEntered(#[debug(skip)] HandlerFnGeneric<DeviceId>),
+    OnCursorLeft(#[debug(skip)] HandlerFnGeneric<DeviceId>),
+    OnThemeChanged(#[debug(skip)] HandlerFnGeneric<winit::window::Theme>),
+    OnKeyboardInput(#[debug(skip)] HandlerFnGeneric<OnKeyboardInput>),
+    OnModifiersChanged(#[debug(skip)] HandlerFnGeneric<Modifiers>),
 }
 
 #[derive(Debug)]
 pub enum UnregisterWindowEventHandlerType {
     Widget(Option<WidgetId>),
     OnDestroy,
+    OnFocused,
+    OnOccluded,
+    OnCursorEntered,
+    OnCursorLeft,
+    OnThemeChanged,
+    OnKeyboardInput,
+    OnModifiersChanged,
 }
 
 #[derive(Debug)]
@@ -41,18 +70,20 @@ pub enum HandleEvent<'a> {
         widget_id: WidgetId,
         action: &'a ErasedAction,
     },
-    OnDestroy,
+    WindowEvent(&'a WindowEvent),
 }
 
 #[derive(Default)]
 pub(crate) struct WindowEventHandlers {
     widget_handlers: HashMap<WidgetId, EventMap<HandlerFn>>,
     on_destroy_handler: EventMap<NoParamHandlerFn>,
-    // TODO add on_mouseenter for widgets
-    // TODO add on_mouseexit for widgets
-    // TODO add on_keydown for window
-    // TODO add on_keyup for window
-    // TODO add on_device_event for window
+    on_focused_handler: EventMap<HandlerFnGenericStatic<bool>>,
+    on_occluded_handler: EventMap<HandlerFnGenericStatic<bool>>,
+    on_cursor_entered_handler: EventMap<HandlerFnGeneric<DeviceId>>,
+    on_cursor_left_handler: EventMap<HandlerFnGeneric<DeviceId>>,
+    on_theme_changed_handler: EventMap<HandlerFnGeneric<winit::window::Theme>>,
+    on_keyboard_input_handler: EventMap<HandlerFnGeneric<OnKeyboardInput>>,
+    on_modifiers_changed_handler: EventMap<HandlerFnGeneric<Modifiers>>,
 }
 
 impl WindowEventHandlers {
@@ -65,9 +96,56 @@ impl WindowEventHandlers {
                 };
                 handlers.values().for_each(|h| (h)(action));
             }
-            HandleEvent::OnDestroy => {
-                self.on_destroy_handler.values().for_each(|h| (h)());
-            }
+            HandleEvent::WindowEvent(event) => match event {
+                WindowEvent::Destroyed => {
+                    self.on_destroy_handler.values().for_each(|h| (h)());
+                }
+                WindowEvent::Focused(is_focused) => {
+                    self.on_focused_handler
+                        .values()
+                        .for_each(|h| (h)(*is_focused));
+                }
+                WindowEvent::Occluded(is_occluded) => {
+                    self.on_occluded_handler
+                        .values()
+                        .for_each(|h| (h)(*is_occluded));
+                }
+                WindowEvent::CursorEntered { device_id } => {
+                    self.on_cursor_entered_handler
+                        .values()
+                        .for_each(|h| (h)(device_id));
+                }
+                WindowEvent::CursorLeft { device_id } => {
+                    self.on_cursor_left_handler
+                        .values()
+                        .for_each(|h| (h)(device_id));
+                }
+                WindowEvent::ThemeChanged(theme) => {
+                    self.on_theme_changed_handler
+                        .values()
+                        .for_each(|h| (h)(theme));
+                }
+                WindowEvent::KeyboardInput {
+                    device_id,
+                    event,
+                    is_synthetic,
+                } => {
+                    let to_send = OnKeyboardInput {
+                        device_id: *device_id,
+                        event: event.clone(),
+                        is_synthetic: *is_synthetic,
+                    };
+                    self.on_keyboard_input_handler
+                        .values()
+                        .for_each(|h| (h)(&to_send));
+                }
+                WindowEvent::ModifiersChanged(modifiers) => {
+                    self.on_modifiers_changed_handler
+                        .values()
+                        .for_each(|h| (h)(modifiers));
+                }
+                _ => {}
+            },
         }
     }
     pub fn add_handler_fn(&mut self, handler: RegisterWindowEventHandler) {
@@ -80,6 +158,34 @@ impl WindowEventHandlers {
             }
             RegisterWindowEventHandlerType::OnDestroy(handler_fn) => {
                 self.on_destroy_handler
+                    .insert(handler.handler_id, handler_fn);
+            }
+            RegisterWindowEventHandlerType::OnFocused(handler_fn) => {
+                self.on_focused_handler
+                    .insert(handler.handler_id, handler_fn);
+            }
+            RegisterWindowEventHandlerType::OnOccluded(handler_fn) => {
+                self.on_occluded_handler
+                    .insert(handler.handler_id, handler_fn);
+            }
+            RegisterWindowEventHandlerType::OnCursorEntered(handler_fn) => {
+                self.on_cursor_entered_handler
+                    .insert(handler.handler_id, handler_fn);
+            }
+            RegisterWindowEventHandlerType::OnCursorLeft(handler_fn) => {
+                self.on_cursor_left_handler
+                    .insert(handler.handler_id, handler_fn);
+            }
+            RegisterWindowEventHandlerType::OnThemeChanged(handler_fn) => {
+                self.on_theme_changed_handler
+                    .insert(handler.handler_id, handler_fn);
+            }
+            RegisterWindowEventHandlerType::OnKeyboardInput(handler_fn) => {
+                self.on_keyboard_input_handler
+                    .insert(handler.handler_id, handler_fn);
+            }
+            RegisterWindowEventHandlerType::OnModifiersChanged(handler_fn) => {
+                self.on_modifiers_changed_handler
                     .insert(handler.handler_id, handler_fn);
             }
         }
@@ -96,13 +202,7 @@ impl WindowEventHandlers {
         self.widget_handlers.shrink_to_fit();
         self.on_destroy_handler.shrink_to_fit();
     }
-    fn remove_handler_raw(&mut self, handler_id: &HandlerId) -> bool {
-        if self.remove_widget_handler(handler_id, None) {
-            true
-        } else {
-            self.remove_on_destroy_handler(handler_id)
-        }
-    }
+
     fn find_handler_widget_id(&self, handler_id: &HandlerId) -> Option<WidgetId> {
         self.widget_handlers
             .iter()
@@ -110,6 +210,67 @@ impl WindowEventHandlers {
             .map(|(w, _)| w)
             .cloned()
     }
+
+    pub fn remove_handler(
+        &mut self,
+        handler_id: &HandlerId,
+        type_: Option<UnregisterWindowEventHandlerType>,
+    ) -> bool {
+        if let Some(type_) = type_ {
+            match type_ {
+                UnregisterWindowEventHandlerType::Widget(widget_id) => {
+                    self.remove_widget_handler(handler_id, widget_id)
+                }
+                UnregisterWindowEventHandlerType::OnDestroy => {
+                    self.remove_on_destroy_handler(handler_id)
+                }
+                UnregisterWindowEventHandlerType::OnFocused => {
+                    self.remove_on_focused_handler(handler_id)
+                }
+                UnregisterWindowEventHandlerType::OnOccluded => {
+                    self.remove_on_occluded_handler(handler_id)
+                }
+                UnregisterWindowEventHandlerType::OnCursorEntered => {
+                    self.remove_on_cursor_entered_handler(handler_id)
+                }
+                UnregisterWindowEventHandlerType::OnCursorLeft => {
+                    self.remove_on_cursor_left_handler(handler_id)
+                }
+                UnregisterWindowEventHandlerType::OnThemeChanged => {
+                    self.remove_on_theme_changed_handler(handler_id)
+                }
+                UnregisterWindowEventHandlerType::OnKeyboardInput => {
+                    self.remove_on_keyboard_input_handler(handler_id)
+                }
+                UnregisterWindowEventHandlerType::OnModifiersChanged => {
+                    self.remove_on_modifiers_changed_handler(handler_id)
+                }
+            }
+        } else {
+            self.remove_handler_raw(handler_id)
+        }
+    }
+}
+
+impl WindowEventHandlers {
+    fn remove_handler_raw(&mut self, handler_id: &HandlerId) -> bool {
+        macro_rules! handle_remove {
+            ($($func:ident, )*) => {
+                $(
+                    self.$func(handler_id) ||
+                )* false
+            };
+        }
+        // TODO add the others
+        handle_remove!(
+            remove_widget_handler_none,
+            remove_on_destroy_handler,
+            remove_on_focused_handler,
+            remove_on_occluded_handler,
+            remove_on_cursor_entered_handler,
+        )
+    }
+
     fn remove_widget_handler(
         &mut self,
         handler_id: &HandlerId,
@@ -129,26 +290,34 @@ impl WindowEventHandlers {
         }
         true
     }
+    fn remove_widget_handler_none(&mut self, handler_id: &HandlerId) -> bool {
+        self.remove_widget_handler(handler_id, None)
+    }
     fn remove_on_destroy_handler(&mut self, handler_id: &HandlerId) -> bool {
         self.on_destroy_handler.remove(handler_id).is_some()
     }
-    pub(crate) fn remove_handler(
-        &mut self,
-        handler_id: &HandlerId,
-        type_: Option<UnregisterWindowEventHandlerType>,
-    ) -> bool {
-        if let Some(type_) = type_ {
-            match type_ {
-                UnregisterWindowEventHandlerType::Widget(widget_id) => {
-                    self.remove_widget_handler(handler_id, widget_id)
-                }
-                UnregisterWindowEventHandlerType::OnDestroy => {
-                    self.remove_on_destroy_handler(handler_id)
-                }
-            }
-        } else {
-            self.remove_handler_raw(handler_id)
-        }
+    fn remove_on_focused_handler(&mut self, handler_id: &HandlerId) -> bool {
+        self.on_focused_handler.remove(handler_id).is_some()
+    }
+    fn remove_on_occluded_handler(&mut self, handler_id: &HandlerId) -> bool {
+        self.on_occluded_handler.remove(handler_id).is_some()
+    }
+    fn remove_on_cursor_entered_handler(&mut self, handler_id: &HandlerId) -> bool {
+        self.on_cursor_entered_handler.remove(handler_id).is_some()
+    }
+    fn remove_on_cursor_left_handler(&mut self, handler_id: &HandlerId) -> bool {
+        self.on_cursor_left_handler.remove(handler_id).is_some()
+    }
+    fn remove_on_theme_changed_handler(&mut self, handler_id: &HandlerId) -> bool {
+        self.on_theme_changed_handler.remove(handler_id).is_some()
+    }
+    fn remove_on_keyboard_input_handler(&mut self, handler_id: &HandlerId) -> bool {
+        self.on_keyboard_input_handler.remove(handler_id).is_some()
+    }
+    fn remove_on_modifiers_changed_handler(&mut self, handler_id: &HandlerId) -> bool {
+        self.on_modifiers_changed_handler
+            .remove(handler_id)
+            .is_some()
     }
 }
 
@@ -171,7 +340,8 @@ impl Debug for WindowEventHandlers {
                     .map(|(id, handles)| (*id, handles.keys().collect::<Vec<_>>()))
                     .collect::<HashMap<_, _>>(),
             )
-            .finish()
+            // TODO add more attributes
+            .finish_non_exhaustive()
     }
 }
 
@@ -235,3 +405,5 @@ pub fn register_typed_widget_action_listener<W: Widget + 'static, H>(
         }),
     );
 }
+
+// TODO add tests

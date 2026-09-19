4e2358ee108c3569309264e014c9ae92e11f5299
Dirk Tony <tonyramanantsalama@gmail.com>
2026-09-19 12:16:03 +0300

    Use effects for outlets
    Signed-off-by: Dirk Tony <tonyramanantsalama@gmail.com>

--- velona_router/src/components.rs
+++ velona_router/src/components.rs
@@ -4,39 +4,33 @@ use velona_core::{
     masonry_core::core::Widget,
     reactive::{
         effect::Effect,
-        owner::{Owner, expect_context, provide_context},
+        owner::{expect_context, provide_context},
     },
 };
 
 use crate::runtime::{ChildRoute, get_child_route, update_raw_box, use_matched_routes};
 
 pub fn outlet() -> AnyNewWidget {
-    // Prevent double outlet not showing the same thing
-    Owner::current()
-        .expect("`outlet` should be always called inside a `reactive_graph` context")
-        .child()
-        .with(|| {
-            let raw_box = RawBox::empty().prepare();
+    let raw_box = RawBox::empty().prepare();
 
-            let raw_box_ref = raw_box.create_velona_ref();
+    let raw_box_ref = raw_box.create_velona_ref();
+    let child_route = expect_context::<ChildRoute>();
 
-            let child_route = expect_context::<ChildRoute>();
+    let route_id = child_route.route_id;
 
-            let route_id = child_route.route_id;
+    let matches = use_matched_routes();
+    Effect::new(move |_| {
+        provide_context(child_route.params);
+        {
+            let child_route = get_child_route(child_route.index, &matches);
+            provide_context(child_route);
+        }
 
-            let matches = use_matched_routes();
+        let raw_box_ref = raw_box_ref.clone();
 
-            provide_context(child_route.params);
-
-            {
-                let child_route = get_child_route(child_route.index, &matches);
-                provide_context(child_route);
-            }
-
-            Effect::new(move |_| {
-                update_raw_box(&raw_box_ref, &route_id);
-            });
-
-            raw_box.erased()
-        })
+        Effect::new(move || {
+            update_raw_box(&raw_box_ref, &route_id);
+        });
+    });
+    raw_box.erased()
 }

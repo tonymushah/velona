4b78e2fdfc8a1efec9d7a39d7bd832939f338f41
Dirk Tony <tonyramanantsalama@gmail.com>
2026-09-19 10:39:46 +0300

    base example
    Signed-off-by: Dirk Tony <tonyramanantsalama@gmail.com>

--- examples/router_example/src/main.rs
+++ examples/router_example/src/main.rs
@@ -1,3 +1,23 @@
+use velona::{WindowBuilder, masonry::palette::css::WHITE};
+use velona_renderer_vello::create_wgpu_context;
+use velona_router::Router;
+
+fn main_router() -> Router {
+    Router::default()
+}
+
+#[cfg_attr(feature = "hotpath", hotpath::main)]
 fn main() {
-    println!("WIP")
+    env_logger::init();
+    let g_context = create_wgpu_context(None, None);
+    velona::Builder::new(move |_| {
+        velona_renderer_vello::VelloWindowRenderer::new(g_context.clone())
+    })
+    .with_window(
+        WindowBuilder::new(main_router().build())
+            .with_title("Router example")
+            .with_base_color(WHITE),
+    )
+    .run()
+    .unwrap()
 }

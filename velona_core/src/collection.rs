995effaa63c775ca540e54c2fc4a2cffc057d8e0
Dirk Tony <tonyramanantsalama@gmail.com>
2026-09-21 12:59:30 +0300

    fix collections
    Signed-off-by: Dirk Tony <tonyramanantsalama@gmail.com>

--- velona_core/src/collection.rs
+++ velona_core/src/collection.rs
@@ -1,4 +1,5 @@
 use masonry_core::core::{CollectionWidget, NewWidget};
+use reactive_graph::owner::ArenaItem;
 
 use crate::{NewWidgetExt, reactive::effect::Effect, utils::ConsumeResult};
 
@@ -7,7 +8,7 @@ pub type CollectIterItem<P> = (crate::AnyNewWidget, P);
 pub trait NewCollectionWidgetExt<P> {
     fn collect_reactive_iter<I, Ifn>(self, iter_fn: Ifn) -> Self
     where
-        I: IntoIterator<Item = CollectIterItem<P>>,
+        I: IntoIterator<Item = CollectIterItem<P>> + 'static,
         Ifn: Fn() -> I + 'static;
 }
 
@@ -16,22 +17,24 @@ where
     W: CollectionWidget<P> + 'static,
     P: 'static,
 {
+    #[track_caller]
     fn collect_reactive_iter<I, Ifn>(self, iter_fn: Ifn) -> Self
     where
-        I: IntoIterator<Item = CollectIterItem<P>>,
+        I: IntoIterator<Item = CollectIterItem<P>> + 'static,
         Ifn: Fn() -> I + 'static,
     {
         let self_ref = self.create_velona_ref();
         Effect::new(move || {
+            let elements = ArenaItem::new_local(Some(Box::new(iter_fn())));
+
             self_ref
-                .edit_local_now(|mut this| {
+                .edit(move |mut this| {
+                    let Some(elements) = elements.try_update_value(|inner| inner.take()).flatten()
+                    else {
+                        return;
+                    };
                     CollectionWidget::<P>::clear(&mut this);
-                })
-                .consume_with_log_err();
-            let elements = iter_fn();
-            self_ref
-                .edit_local_now(|mut this| {
-                    for (child, param) in elements {
+                    for (child, param) in *elements {
                         CollectionWidget::<P>::add(&mut this, child, param);
                     }
                 })

ab0a9e16ea3ec5b8be668e1e9584651c62daff30
Dirk Tony <tonyramanantsalama@gmail.com>
2026-09-23 11:57:24 +0300

    Update align.rs
    Signed-off-by: Dirk Tony <tonyramanantsalama@gmail.com>

--- velona_masonry_widgets/src/widgets/align.rs
+++ velona_masonry_widgets/src/widgets/align.rs
@@ -12,6 +12,7 @@
 //! [reactive-child]: super::ReactiveSingleChildExt
 
 use masonry::{core::NewWidget, layout::UnitPoint, widgets::Align};
+use velona_core::widgets::View;
 
 use crate::NewWidgetExt;
 
@@ -31,5 +32,38 @@ impl NewAlignExt for NewWidget<Align> {
         self.use_widget_mut(alignment, |mut this, alignment| {
             Align::set_alignment(&mut this, alignment);
         })
+    }
+}
+
+pub trait IntoAlign {
+    fn align_centered(self) -> Align;
+    fn align_right(self) -> Align;
+    fn align_left(self) -> Align;
+    fn align_horizontal(self, align: UnitPoint) -> Align;
+    fn align_vertical(self, align: UnitPoint) -> Align;
+}
+
+impl<V> IntoAlign for V
+where
+    V: View + 'static,
+{
+    fn align_centered(self) -> Align {
+        Align::centered(self.into_new_widget())
+    }
+
+    fn align_right(self) -> Align {
+        Align::right(self.into_new_widget())
+    }
+
+    fn align_left(self) -> Align {
+        Align::left(self.into_new_widget())
+    }
+
+    fn align_horizontal(self, align: UnitPoint) -> Align {
+        Align::horizontal(align, self.into_new_widget())
+    }
+
+    fn align_vertical(self, align: UnitPoint) -> Align {
+        Align::vertical(align, self.into_new_widget())
     }
 }
